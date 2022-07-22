e977bdbb81cd5288987da734952b04102ff665e5
prachimalla <prachimalla7@gmail.com>
2022-07-22 13:55:02 +0530

    1 file modified

--- index.js
+++ index.js
@@ -112,32 +112,112 @@ console.log(patten);
 var searchInsert = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > target) {
-     // nums.splice(i, 0, target);
+      // nums.splice(i, 0, target);
     }
   }
   console.log(nums);
 };
 //searchInsert([1, 3, 4, 7, 8], 5);
 
-
-var lengthOfLongestSubstring = function(s) {
-  let subStr = []; 
-   for(let i=0;i<s.length;i++){
-    console.log(subStr )
-       if(subStr.length != 0){
-           for(let j = 0;j<subStr.length;j++){
-               if(s[i] == subStr[j] ){
-                console.log('subStr',subStr,j)
-               subStr.splice(0,j+1);
-              
-               }
-     } 
-     subStr.push(s[i] ) 
-       }else {
-        subStr.push(s[i] ) 
-        
-       }
-   }
-   return subStr;
+var lengthOfLongestSubstring = function (s) {
+  let subStr = [];
+  for (let i = 0; i < s.length; i++) {
+    console.log(subStr);
+    if (subStr.length != 0) {
+      for (let j = 0; j < subStr.length; j++) {
+        if (s[i] == subStr[j]) {
+          console.log('subStr', subStr, j);
+          subStr.splice(0, j + 1);
+        }
+      }
+      subStr.push(s[i]);
+    } else {
+      subStr.push(s[i]);
+    }
+  }
+  return subStr;
 };
-console.log(lengthOfLongestSubstring('abcabbc'))
+console.log(lengthOfLongestSubstring('abcabbc'));
+
+const myArr = [1, 2, 3, 1, 3, 4, 5];
+
+let newArr = Array.from(new Set(myArr));
+console.log(newArr);
+
+function avgArray(myArr) {
+  let sum = 0;
+  for (let a = 0; a < myArr.length; a++) {
+    sum += myArr[a];
+  }
+  return sum / myArr.length;
+}
+
+console.log(avgArray(myArr));
+const abc = 'Well, how, are , we , doing, today';
+function sentence(abc) {
+  console.log(abc.split(',').join(' '));
+}
+sentence(abc);
+
+const array1 = ['abc', 'dd', 'iou'];
+const array2 = ['abc', 'dd', 'iou'];
+const array3 = [...array1, ...array2];
+
+array3.splice(3, 0, 'item');
+// let array4 = array3.map(ele => {
+//   if(ele == 'item'){
+//     return "prachi"
+//   }
+//   return ele;
+// })
+let array4 = array3.indexOf('item');
+array3[array4] = 'prachi';
+console.log(array3);
+let isinclude = array1.every((ele) => {
+  return array2.includes(ele);
+});
+console.log(isinclude);
+
+function anagram() {
+  let str1 = 'prachi';
+  let str2 = 'parhsi';
+  if (str1.length != str2.length) {
+    return false;
+  }
+  str1 = Array.from(str1).sort();
+  str2 = Array.from(str2).sort();
+  for (let a = 0; a < str1.length; a++) {
+    if (str1[a] !== str2[a]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(anagram());
+
+const arrOfStr = ['prachi', 'malla', 'ram'];
+
+arrOfStr.slice(1, 'abc');
+console.log('arrOfStr', arrOfStr);
+
+function revEachWord(arrOfStr) {
+  const newArr = [];
+  for (let i = 0; i < arrOfStr.length; i++) {
+    newArr.push(arrOfStr[i].split('').reverse().join(''));
+  }
+  return newArr;
+}
+console.log(revEachWord(arrOfStr));
+
+function palindromeNum(num) {
+  let newNum = '';
+  for (let i = num.length - 1; i >= 0; i--) {
+    console.log(i);
+    newNum += num[i];
+  }
+  if (newNum == num) {
+    return true;
+  }
+  return false;
+}
+console.log(palindromeNum('12321'));
